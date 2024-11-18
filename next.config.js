6ee09a4a81e1ab7139ccca8a418336fae283e6f1
blagoySimandov <baggy.simandoff@gmail.com>
2024-11-18 01:54:05 +0000

    add CORS headers

--- next.config.js
+++ next.config.js
@@ -1,18 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { 
+  images: {
     unoptimized: true,
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" }, //TODO: Change this to the domain
+            {
+              key: "Access-Control-Allow-Methods",
+              value: "GET,DELETE,PATCH,POST,PUT",
+            },
+            {
+              key: "Access-Control-Allow-Headers",
+              value:
+                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            },
+          ],
+        },
+      ];
+    },
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
 };
 
 module.exports = nextConfig;
+
