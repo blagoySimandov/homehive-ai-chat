abb32277566ad5ce0d9950a65ed243aa0a2a080b
blagoySimandov <baggy.simandoff@gmail.com>
2024-11-18 01:59:34 +0000

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, //TODO: Change this to the domain
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
     unoptimized: true,
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" }, //TODO: Change this to the domain
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,DELETE,PATCH,POST,PUT",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            },
-          ],
-        },
-      ];
-    },
     remotePatterns: [
       {
         protocol: "https",
